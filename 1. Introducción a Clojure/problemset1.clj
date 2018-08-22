96c88e9738b70cc3f9a8aff3b97a5851c3f7833d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-08-22 22:36:08 +0000

    Ejemplos usando loop.

--- 1. Introducción a Clojure/problemset1.clj
+++ 1. Introducción a Clojure/problemset1.clj
@@ -11,9 +11,34 @@
         5)
      9))
 
+(defn add-list
+  "Returns the sum of all the elements of its input
+  list, or 0 if its empty. Assume that all the
+  elements in the input list are numbers."
+  [lst]
+  (loop [sum 0
+         lst lst]
+    (if (empty? lst)
+      sum
+      (recur (+ sum (first lst)) (rest lst)))))
+
+(defn fact
+  "Computes the factorial of n."
+  [n]
+  (loop [i      1
+         result 1]
+    (if (> i n)
+      result
+      (recur (inc i) (*' result i)))))
+
 (deftest test-f2c
   (is (= 100.0 (f2c 212.0)))
   (is (= 0.0 (f2c 32.0)))
   (is (= -40.0 (f2c -40.0))))
+
+(deftest test-add-list
+  (is (= 0 (add-list ())))
+  (is (= 10 (add-list '(2 4 1 3))))
+  (is (= 55 (add-list '(1 2 3 4 5 6 7 8 9 10)))))
 
 (run-tests)
